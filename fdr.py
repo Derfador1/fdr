b9dc641525d772200624bc10e35367f652871039
Derfador1 <Derfador1@gmail.com>
2016-06-30 15:28:45 -0400

    added a basis for each function of the program

---
+++ fdr.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+def fibonacci(n):
+	a = 0
+	b = 1
+	for i in range(0, n):
+		temp = a
+		a = b
+		b = temp + b
+	return hex(a)
+
+def hex_to_dec(n):
+	return hex(n)
+	
+    
+def roman_to_arabic(number):
+	numerals = [
+		{'letter': 'M', 'value': 1000},
+		{'letter': 'D', 'value': 500},
+		{'letter': 'C', 'value': 100},
+		{'letter': 'L', 'value': 50},
+		{'letter': 'X', 'value': 10},
+		{'letter': 'V', 'value': 5},
+		{'letter': 'I', 'value': 1},
+	]
+	
+	
+	index_by_letter = {}
+	for index in range(len(numerals)):
+		index_by_letter[numerals[index]['letter']] = index
+
+	result = 0
+	previous_value = None
+	for letter in reversed(number):
+		index = index_by_letter[letter]
+		value = numerals[index]['value']
+		if (previous_value is None) or (previous_value <= value):
+			result += value
+		else:
+			result -= value
+		previous_value = value
+
+	return hex(result)
+	
+def main():
+	x = fibonacci(10)
+	print(x)
+	
+	b = hex_to_dec(10998723)
+	print(b)
+	
+	d = 'MDCDIIII'
+	
+	c = roman_to_arabic(d)
+	print(c)
+
+if __name__ == '__main__':
+	main()
